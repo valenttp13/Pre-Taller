1c716c5b47d75ced52e3bbdaa0338cbb6b67aa79
FelipeCardona194 <cardonafelipe293@gmail.com>
2024-08-31 13:30:10 -0500

    models

--- Biblioteca_Digital/App/models.py
+++ Biblioteca_Digital/App/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=200)
+    fecha_publicacion = models.DateField()
+    genero = models.CharField(max_length=100)
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.titulo
